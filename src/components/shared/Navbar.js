93c70892d8a6195af515cbe5d8a5a65929b33162
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2024-05-06 23:16:38 +0600

    Active Routing Highlight

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import logo from "@/assets/Logo.png";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,41 +7,48 @@ import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Image from 'next/image';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import ProfileMenu from "../ui/ProfileMenu";
 
 
-const  Navbar = () => {
+const menuItems = [
+    {name: "Home", href:"/"},
+    {name: "Program", href:"/program"},
+    {name: "Stories", href:"/stories"},
+    {name: "Blog", href:"/blog"},
+    {name: "About", href:"/about"},
+]
 
-  return (
-    <>
-        <AppBar position="sticky" className="bg-green-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Link href="/">
-                        <Image src={logo} width={100} height={100} alt="Logo" />
-                    </Link>
-                <Box className="w-full text-right">
-                    <Link href="/">
-                        <Button className="text-black">Home</Button>
-                    </Link>
-                    <Link href="/program">
-                        <Button className="text-black">Program</Button>
-                    </Link>
-                    <Link href="/stories">
-                        <Button className="text-black">Stories</Button>
-                    </Link>
-                    <Link href="/blog">
-                        <Button className="text-black">Blog</Button>
-                    </Link>
-                    <Link href="/about">
-                        <Button className="text-black">About</Button>
-                    </Link>
-                </Box>
-                <ProfileMenu/>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    </>
-  );
+const Navbar = () => {
+
+    const pathname = usePathname();
+    
+    return (
+        <>
+            <AppBar position="sticky" className="bg-green-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Link href="/">
+                            <Image src={logo} width={100} height={100} alt="Logo" />
+                        </Link>
+                        <Box className="w-full text-right">
+                            {
+                                menuItems.map((link)=>{
+                                    const isActive = pathname.startsWith(link.href);
+                                    return (
+                                        <Link href={link.href} key={link.name} >
+                                              <Button className={isActive ? "text-green-500" : "text-red-600"} >{link.name}</Button>
+                                        </Link>
+                                    );
+                                })
+                            }
+                        </Box>
+                        <ProfileMenu />
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    );
 }
+
 export default Navbar;
